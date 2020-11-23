f3c4e4ddd6b6a33631d740c9f22e70e37e88484f
vinaybana <vinay@securitytroops.com>
2020-11-23 16:55:22 +0530

    script for ipindia updated

--- scripts/certi.py
+++ scripts/certi.py
@@ -10,6 +10,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import Select
+import requests
+import json
 
 # prefs = {"download.default_directory" : '/var/www/seleniumtest/archive/'}
 def get_source(html):
@@ -25,52 +27,68 @@ options.add_argument("--remote-debugging-port=9222")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument("--silent")
 
+apiurl = "http://13.127.92.196:8000/cvakilapi/v1/company/"
 browser = webdriver.Chrome('/var/www/chromedriver', options=options)
+while True:
+	Cinlist = requests.get(url = apiurl+"?scrap_status=0&status=Active")
+	cin_list = json.loads(Cinlist.text)
+	for i in cin_list['results']:
+		browser.get('http://www.mca.gov.in/mcafoportal/getCertifiedCopies.do')
+		time.sleep(5)
 
-browser.get('http://www.mca.gov.in/mcafoportal/getCertifiedCopies.do')
-time.sleep(5)
+		checkbox = browser.find_element_by_id("cinChk")
+		checkbox.click()
+		CINinput = browser.find_element_by_id("cinFDetails")
+		CINinput.send_keys(i['cin']) 
+		print(i['cin'])
 
-checkbox = browser.find_element_by_id("cinChk")
-checkbox.click()
-CINinput = browser.find_element_by_id("cinFDetails")
-CINinput.send_keys("U25191KA1982PTC004894") 
-submit = browser.find_element_by_id("submitBtn")
-submit.click()
-browser.find_element_by_link_text("U25191KA1982PTC004894").click()
-time.sleep(2)
-html = browser.page_source
-soup = get_source(html)
-opt = soup.find(id='searchCategoryDetails_categoryName').text.split('\n')
-docs = opt[2:-1]
-y = soup.find(id='searchCategoryDetails_finacialYear').text.split('\n')
-years = y[2:-1]
-
-for doc in docs:
-	browser.find_element_by_xpath("//select[@id='searchCategoryDetails_categoryName']").send_keys(doc)
-	time.sleep(2)
-	for year in years:
-		browser.find_element_by_xpath("//select[@id='searchCategoryDetails_finacialYear']").send_keys(year)
-		time.sleep(1)
-		browser.find_element_by_id("searchCategoryDetails_0").click()
-		time.sleep(3)
-		try:
-			# error = browser.find_element_by_class_name("error_overlay").text
-			browser.find_element_by_id("msgboxclose").click()
-			continue
-		except:
-			pass
+		submit = browser.find_element_by_id("submitBtn")
+		submit.click()
+		browser.find_element_by_link_text(i['cin']).click()
+		time.sleep(2)
 		html = browser.page_source
 		soup = get_source(html)
+		opt = soup.find(id='searchCategoryDetails_categoryName').text.split('\n')
+		docs = opt[2:-1]
+		y = soup.find(id='searchCategoryDetails_finacialYear').text.split('\n')
+		years = y[2:-1]
 
+		for doc in docs:
+			browser.find_element_by_xpath("//select[@id='searchCategoryDetails_categoryName']").send_keys(doc)
+			doc_category = doc;
+			time.sleep(2)
+			for year in years:
+				if year > '2020':
+					break
+				doc_year = year;
+				browser.find_element_by_xpath("//select[@id='searchCategoryDetails_finacialYear']").send_keys(year)
+				time.sleep(1)
+				browser.find_element_by_id("searchCategoryDetails_0").click()
+				time.sleep(3)
+				try:
+					browser.find_element_by_id("msgboxclose").click()
+					continue
+				except:
+					pass
+				html = browser.page_source
+				soup = get_source(html)
+				document = soup.find(id='results').tbody
+				for doc in document.find_all('tr')[1:]:
+					name = doc.find_all('td')[1].text.strip()
+					d = doc.find_all('td')[2].text.strip().split("/")
+					date = "-".join(reversed(d))
+					try:
+						data = {'company':i['id'],'category':doc_category, 'name':name,'year': doc_year,'date':date,'status':'Active'}
+						req = requests.post(url="http://13.127.92.196:8000/cvakilapi/v1/document/",data=data)
+						if req.status_code == 201 or req.status_code == 200:
+							data2 = {'scrap_status':'1'}
+							requ = requests.patch(url="http://13.127.92.196:8000/cvakilapi/v1/company/"+str(i["id"])+"/",data=data2 )
+							print(requ.status_code, requ.text)	
+						print(req.status_code, req.text)
+						
+					except Exception as e:
+						print(e)					
 
-		document = soup.find(id='results').tbody
-
-		for doc in document.find_all('tr')[1:]:
-			print(doc.find_all('td')[1].text.strip())
-			print(doc.find_all('td')[2].text.strip())
-			print(doc.find_all('td')[3].text.strip())
-			print("\n")
-
-browser.quit()
+	browser.quit()
 
 
--- scripts/indiaservices.py
+++ scripts/indiaservices.py
@@ -44,7 +44,7 @@ def getDetail(browser,img_name):
 
 # download_dir = {"download.default_directory" : '/var/www/Python-projects/python-script/download/ipindiadocs/'}
 # profile = {"plugins.plugins_list": [{"enabled": True, "name": "Chrome PDF Viewer"}], # Disable Chrome's PDF Viewer
-               # "download.default_directory": '/var/www/Python-projects/python-script/download/ipindiadocs/', "download.extensions_to_open": "applications/pdf"}
+			   # "download.default_directory": '/var/www/Python-projects/python-script/download/ipindiadocs/', "download.extensions_to_open": "applications/pdf"}
 options = Options()			
 options.add_argument("--start-maximized")
 options.add_argument('--ignore-certificate-errors')
@@ -53,11 +53,11 @@ options.add_argument("--remote-debugging-port=9222")
 # options.add_experimental_option("prefs",prefs)
 # options.add_experimental_option("prefs", profile)
 options.add_experimental_option('prefs',  {
-    "download.default_directory": '/var/www/Python-projects/python-script/download/ipindiadocs/',
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "plugins.always_open_pdf_externally": True
-    }
+	"download.default_directory": '/var/www/Python-projects/python-script/download/ipindiadocs/',
+	"download.prompt_for_download": False,
+	"download.directory_upgrade": True,
+	"plugins.always_open_pdf_externally": True
+	}
 )
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument("--silent")
@@ -74,56 +74,105 @@ a =	browser.find_element_by_xpath("//p[contains(text(), 'Total Document(s):')]")
 total_count = a.split(' ')[2]
 rows = len(browser.find_elements_by_xpath("//table/tbody/tr"))
 total_page = (int(total_count)//int(rows))+1
-for i in range(total_page):
-	trs = browser.find_elements_by_xpath("//table/tbody/tr/td[1]")
-	for i in trs:
-		i.click()
-		browser.switch_to.window(browser.window_handles[1])
-		soup = get_source(browser.page_source)
+
+try:
+	for i in range(total_page):
+		trs = browser.find_elements_by_xpath("//table/tbody/tr/td[1]")
+		for i in trs:
+			i.click()
+			browser.switch_to.window(browser.window_handles[1])
+			soup = get_source(browser.page_source)
+			
+			get_data = soup.find(id='home').table.tbody.find_all('tr')
 		
-		get_data = soup.find(id='home').table.tbody.find_all('tr')
-	
-		key = []
-		value = []
-		for data in get_data:
-			try:
-				key.append(data.find_all('td')[0].text.strip())
-				value.append(data.find_all('td')[1].text.strip())
-			except Exception as e:
-				pass
+			key = []
+			value = []
+			for data in get_data:
+				try:
+					key.append(data.find_all('td')[0].text.strip())
+					value.append(data.find_all('td')[1].text.strip())
+				except Exception as e:
+					pass
 
-			if data.find_all('td')[0].text == "Inventor":
-				break
-		print(key.pop(-1),value)
+				if data.find_all('td')[0].text == "Inventor":
+					break
+			key.pop(-1)
+			all_data = dict(zip(key, value))
+			tables = soup.find('table',{"class": "table-striped"}).find_all('table',{"class": "table-striped"})
+			inventory_data = []
+			inventory = {}
+			inventory_key = {}
+			for table in tables[0].tbody.find_all('tr')[1:]:
+				inventory_key['Name'] = table.find_all('td')[0].text
+				inventory_key['Address'] = table.find_all('td')[1].text
+				inventory_key['Country'] = table.find_all('td')[2].text
+				inventory_key['Nationality'] = table.find_all('td')[3].text
+				inventory_data.append(inventory_key)
+			all_data['Inventory'] = inventory_data
+			applicant_data = []
+			applicant = {}
+			applicant_key = {}
+			for table in tables[1].tbody.find_all('tr')[1:]:
+				applicant_key['Name'] = table.find_all('td')[0].text
+				applicant_key['Address'] = table.find_all('td')[1].text
+				applicant_key['Country'] = table.find_all('td')[2].text
+				applicant_key['Nationality'] = table.find_all('td')[3].text
+				applicant_data.append(applicant_key)
+			all_data['Applicant'] = applicant_data
+			for data in get_data:
+				try:
+					if "Abstract" in data.find_all('td')[0].text:
+						all_data['Abstract'] = data.find_all('td')[0].text.split(':')[1].strip()
+				except Exception as e:
+					continue
+			for data in get_data:
+				try:
+					if "Complete Specification" in data.find_all('td')[0].text:
+						all_data['Complete Specification'] = data.find_all('td')[0].text.strip()
+				except Exception as e:
+					continue
+			browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+			browser.find_element_by_name("submit").click()
+			time.sleep(5)
+			browser.switch_to.window(browser.window_handles[2])
+			soup = get_source(browser.page_source)
+			app_data = soup.find(id='Content').table.tbody.find_all('tr')
+			detail_key = []
+			detail_value = []
+			for data in app_data:
+				try:
+					detail_key.append(data.find_all('td')[0].text.strip())
+					detail_value.append(data.find_all('td')[1].text.strip())
+				except Exception as e:
+					pass
+			
+			a = browser.find_element_by_xpath("//table/tbody/tr/td/h3/strong").text
+			detail_key.append('Application Status')
+			detail_value.append(a)
+			detail_data = dict(zip(detail_key, detail_value))
+			new1_detail_data = {detail_key:detail_value for detail_key, detail_value in detail_data.items() if detail_key != 'APPLICATION NUMBER'}
+			new2_detail_data = {detail_key:detail_value for detail_key, detail_value in new1_detail_data.items() if detail_key != 'APPLICANT NAME'}
+			new3_detail_data = {detail_key:detail_value for detail_key, detail_value in new2_detail_data.items() if detail_key != 'TITLE OF INVENTION'}
+			new_detail_data = {detail_key:detail_value for detail_key, detail_value in new3_detail_data.items() if detail_key != 'FIELD OF INVENTION'} 
+			total_data = dict(list(all_data.items()) + list(new_detail_data.items()))
+			with open(".json", "w") as outfile: 
+				json.dump(total_data, outfile)
+			browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+			browser.find_element_by_name("SubmitAction").click()
+			time.sleep(5)
 
-		tables = soup.find('table',{"class": "table-striped"}).find_all('table',{"class": "table-striped"})
-		for table in tables:
-			print(table[0].text)
-		# trows = browser.find_elements_by_xpath("//table/tbody/tr")
-		# data = {}
-		# for l in trows:
-		# 	print(l.text)
-		# 	key = browser.find_element_by_xpath("//table/tbody/tr/td[1]").text
-		# 	value = browser.find_element_by_xpath("//table/tbody/tr/td[2]").text
-		# 	data[key] = value
-		# 	print(data)
-			# app = json.loads(data)
-		# browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		# browser.find_element_by_name("submit").click()
-		# time.sleep(5)
-		# browser.switch_to.window(browser.window_handles[2])
-		# browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		# browser.find_element_by_name("SubmitAction").click()
-		# time.sleep(5)
-		# doctrs = browser.find_elements_by_xpath("//table/tbody/tr/td[1]")
-		# for j in doctrs:
-		# 	j.click()
-		# 	time.sleep(5)
-		# browser.close()
-		# browser.switch_to.window(browser.window_handles[1])
-		# browser.close()
-		# browser.switch_to.window(browser.window_handles[0])
+			btns = browser.find_elements_by_name("DocumentName")
+			print(btns)
 
+			for btn in btns:
+				btn.click()
+				time.sleep(5)
+			browser.close()
+			browser.switch_to.window(browser.window_handles[1])
+			browser.close()
+			browser.switch_to.window(browser.window_handles[0])
+except Exception as e:
+	print(e)
 
 time.sleep(5)
 browser.quit()
